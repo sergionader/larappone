43bdefc18e989cdc803fea7bdf045348f644222d
Sergio <sergio.nader@gmail.com>
2018-01-04 13:13:18 -0500

    added github lilnk to the footer

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -2,8 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <!-- <a href="https://bitbucket.org/sergionader/larappone" target="_blank">BitBucket Repo</a> -->
-        Git will be back soon!
+        <a href="https://github.com/sergionader/larappone" target="_blank">GitHub</a>
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2017 <a href="http://thedevproject.info" target="_blank">thedevproject.com</a> - </strong> <a href="{{route('docs.author.index')}}">Sergio Nader</a>
